e17a63cdcc4d2360afa550509accf8836e58f6b3
Wing kuk Cheng <>
2023-02-03 11:08:40 +0800

    about & teacher part finished

--- teachers/admin.py
+++ teachers/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Teacher
+
+
+class TeacherAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    list_display_links = ('id', 'name')
+    list_filter = ('name',)
+    search_fields = ('id', 'title', 'description')
+    list_per_page = 20
+
+
+admin.site.register(Teacher, TeacherAdmin)
